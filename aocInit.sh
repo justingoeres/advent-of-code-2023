d9f17d41dca07458dfacde23b0cc055824efcff3
Justin Goeres <justin.goeres@merklecxm.com>
2023-11-30 19:53:04 -0500

    Set year to 2023

--- aocInit.sh
+++ aocInit.sh
@@ -120,7 +120,7 @@ function main() {
   colour_init
   #lock_init system
 
-  YEAR=2021
+  YEAR=2023
   puzzleInputFile="data/day$day/input.txt"
   srcDir=src/main/java/org/jgoeres/adventofcode$YEAR/Day$day
   testDir=src/test/java/org/jgoeres/adventofcode$YEAR
