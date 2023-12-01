4d1b504eddd067a863f3963d33df2c5e3dcc157b
Justin Goeres <justin.goeres@merklecxm.com>
2023-12-01 07:10:46 -0500

    Day 1 Init from aocInit.sh

---
+++ src/test/java/org/jgoeres/adventofcode2023/Day01Test.java
@@ -0,0 +1,87 @@
+package org.jgoeres.adventofcode2023;
+
+import org.jgoeres.adventofcode2023.Day01.Day01Service;
+import org.jgoeres.adventofcode.common.ToClipboard;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Day01Test {
+    // Puzzle
+    private final String PUZZLE_INPUT = "data/day01/input.txt";
+    private final boolean PUZZLE_DEBUG = false;
+    private Day01Service day01Service = null;
+
+    // Examples
+    private final String EXAMPLE1_INPUT = "data/day01/example1.txt";
+    private final boolean EXAMPLE_DEBUG = false;
+    private Day01Service example1Service = null;
+
+    @Test
+    @Order(1)   // Run before Puzzle Part B
+    public void Day01A() {
+        if (day01Service == null) {
+            day01Service = new Day01Service(PUZZLE_INPUT, PUZZLE_DEBUG);
+        }
+
+        final long EXPECTED = 0;
+        long result = 0;
+        try {
+            result = day01Service.doPartA();
+            ToClipboard.set(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        assertEquals(EXPECTED, result);
+    }
+
+    @Test
+    @Order(2)   // Run after Puzzle Part A
+    public void Day01B() {
+        if (day01Service == null) {
+            day01Service = new Day01Service(PUZZLE_INPUT, PUZZLE_DEBUG);
+        }
+
+        final long EXPECTED = 0;
+        long result = 0;
+        try {
+            result = day01Service.doPartB();
+            ToClipboard.set(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        assertEquals(EXPECTED, result);
+    }
+
+    @Test
+    @Order(3)   // Run before Example Part B
+    @Disabled
+    public void Day01AExample1() {
+        example1Service = new Day01Service(EXAMPLE1_INPUT, EXAMPLE_DEBUG);
+        final long EXPECTED = 0;
+        long result = 0;
+        try {
+            result = example1Service.doPartA();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        assertEquals(EXPECTED, result);
+    }
+
+    @Test
+    @Order(4)   // Run after Example Part A
+    @Disabled
+    public void Day01BExample1() {
+        // Instantiate the service if Part A was skipped
+        if (example1Service == null) example1Service = new Day01Service(EXAMPLE1_INPUT, EXAMPLE_DEBUG);
+
+        final long EXPECTED = 0;
+        long result = 0;
+        try {
+            result = example1Service.doPartB();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        assertEquals(EXPECTED, result);
+    }
+}
