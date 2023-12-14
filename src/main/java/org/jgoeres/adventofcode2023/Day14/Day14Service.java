c524a7f636fb6f36d9ee82f5ba4b8ff9d901fe13
Justin Goeres <justin.goeres@merklecxm.com>
2023-12-14 06:57:17 -0500

    Day 14 Init from aocInit.sh

---
+++ src/main/java/org/jgoeres/adventofcode2023/Day14/Day14Service.java
@@ -0,0 +1,62 @@
+package org.jgoeres.adventofcode2023.Day14;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day14Service {
+    public boolean DEBUG = false;
+
+    private ArrayList<Integer> inputList = new ArrayList<>();
+
+    public Day14Service(String pathToFile) {
+        loadInputs(pathToFile);
+    }
+
+    public Day14Service(String pathToFile, boolean debug) {
+        loadInputs(pathToFile);
+        DEBUG = debug;
+    }
+
+    public long doPartA() {
+        System.out.println("=== DAY 14A ===");
+
+        long result = 0;
+        /** Put problem implementation here **/
+
+        System.out.println("Day 14A: Answer = " + result);
+        return result;
+    }
+
+    public long doPartB() {
+        System.out.println("=== DAY 14B ===");
+
+        long result = 0;
+        /** Put problem implementation here **/
+
+        System.out.println("Day 14B: Answer = " + result);
+        return result;
+    }
+
+    // load inputs line-by-line and extract fields
+    private void loadInputs(String pathToFile) {
+        inputList.clear();
+        try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
+            String line;
+            /** Replace this regex **/
+            Pattern p = Pattern.compile("([FB]{7})([LR]{3})");
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                Matcher m = p.matcher(line);
+                if (m.find()) { // If our regex matched this line
+                    // Parse it
+                    String field1 = m.group(1);
+                    String field2 = m.group(2);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+}
